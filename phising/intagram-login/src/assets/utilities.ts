65bfb28b95cb95276fff6bb5eb97a36f87e81db3
karlosvas <carlosvassan@gmail.com>
2025-01-27 13:52:29 +0100

    Removed old files

--- phising/intagram-login/src/assets/utilities.ts
+++
@@ -1,7 +0,0 @@
-export function redirectUserToInstagram() {
-  const redirectUrl = "https://help.instagram.com/";
-  const ancore = document.createElement("a");
-  ancore.href = redirectUrl;
-  ancore.rel = "noopener noreferrer";
-  ancore.click();
-}
